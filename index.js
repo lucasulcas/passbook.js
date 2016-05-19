359883ef27d76cb571b8cf9f69ba27052601ba8b
Lucas Costa Oliveira <lcosta@br.ibm.com>
2016-05-19 00:57:22 -0300

    Added some stream options

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-exports.createPassbook = function(type, resources, keys, password, passname, callback) {
+exports.streamPassbook = function(type, pass, keys, password, passname, callback) {
 	var passbook = require('./lib/index.js');
 	var fs = require("fs");
-	var obj;
-	fs.readFile(resources+'pass.json', 'utf8', function (err, data) {
-  		if (err) throw err;
-  		console.log('pass.json file redaded successfully');
-  		obj = JSON.parse(data);
-  		var template = passbook.createTemplate(type, {
-		passTypeIdentifier: obj.passTypeIdentifier,
-		teamIdentifier:     obj.teamIdentifier,
-		organizationName:   obj.organizationName
-		}, {
+
+	var template = passbook.createTemplate(type, {
+		passTypeIdentifier: pass.passTypeIdentifier,
+		teamIdentifier:     pass.teamIdentifier,
+		organizationName:   pass.organizationName
+	}, {
 		certs: {
-		  wwdr: keys+'/wwdr.pem',
-		  pass: keys+'/wallet.pem', // pem with certificate and private key
-		  password: password // pass phrase for the pass_cert.pem file
+	  		wwdr: keys+'/wwdr.pem',
+	  		pass: keys+'/wallet.pem', // pem with certificate and private key
+	  		password: password // pass phrase for the pass_cert.pem file
 		}
-		})
-		var pass = template.createPass(obj);
-
-
-		// var stream = pass.pipe(fs.createWriteStream(passname + '.pkpass'));
-		// stream.on('finish',function(){
-		// 	callback();
-		// });
-
-		callback(pass);
 	});
+	var stream = template.createPass(pass);
+	callback(stream);
 }
 
 
